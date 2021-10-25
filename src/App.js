1fd45aa0b5961c66eafa86a4dd82054c198fda7d
nwb43 <arata.hatori@i-tem.info>
2021-10-25 12:33:03 +0900

    useEffectの使い方まで

--- src/App.js
+++ src/App.js
@@ -2,13 +2,19 @@ import React from 'react'
 import logo from './logo.svg';
 import './App.css';
 import Basic from './components/Basic'
+import Basic2 from './components/Basic2';
+import BasicUseEffect from './components/BasicUseEffect';
+import  TimerContainer from './components/TimerContainer';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <Basic />
+         <Basic2 />
+        <BasicUseEffect />
+        <TimerContainer/>
+
       </header>
     </div>
   );
