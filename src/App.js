8ac861c66d582a3273f7cd6114d1378b1b9488b9
nwb43 <arata.hatori@i-tem.info>
2021-10-25 14:20:34 +0900

    Fetch axios の使い方

--- src/App.js
+++ src/App.js
@@ -5,15 +5,17 @@ import Basic from './components/Basic'
 import Basic2 from './components/Basic2';
 import BasicUseEffect from './components/BasicUseEffect';
 import  TimerContainer from './components/TimerContainer';
+import ApiFetch from './components/ApiFetch'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-         <Basic2 />
+{/*          <Basic2 />
         <BasicUseEffect />
-        <TimerContainer/>
+        <TimerContainer/> */}
+        <ApiFetch/>
 
       </header>
     </div>
